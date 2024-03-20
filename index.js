8f940b56f7f9f4fd8460ac6f28e7c36330d1e906
nqduy1805 <tnu1805@gmail.com>
2024-03-20 15:47:43 +0700

    uud

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function display(name){
-    console.log(name)
-    return name;
-}
-display("api-chat-skye")
+var addTwoNumbers = function(l1, l2) {
+    let n1="",n2="";
+    while(l1||l2){
+        if(l1){
+            n1=l1.val+n1;
+            l1=l1.next;
+        }
+        if(l2){
+            n2=l2.val+n2;
+            l2=l2.next;
+        }
+    }
+
+    n1=(BigInt(n1)+BigInt(n2)).toString().split('').map(Number);
+            console.log(n1);
+
+    let c= new ListNode(n1[n1.length -1]);
+    let d = c;
+    for(let  i = n1.length -2 ;i>=0;i--){
+        d.next= new ListNode(n1[i]);
+        d=d.next;
+    }
+    return c;
+};
+console.log(addTwoNumbers([2,4,3],[5,2,9]));
